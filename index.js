24f6fbaf02976931e56c073506dc7dbbb12ecf71
Yasamin <yasamin.amini98@gmail.com>
2024-08-09 14:48:27 +0200

    More JS

---
+++ index.js
@@ -0,0 +1,22 @@
+const quotes = [
+    “Well-behaved women seldom make history.”,
+    “Who knows what women can be when they are finally free to become themselves",
+    “Each time a woman stands up for herself, without knowing it possibly, without claiming it, she stands up for all women.”,
+    “When someone shows you who they are, believe them the first time.” ,
+    “Love costs all we are and will ever be. Yet it is only love which sets us free.”
+]
+
+const usedIndexes = new Set()
+const quoteElement = document.getElementById("quote")
+function generateQuote(){
+   while (true)  {    
+    const randomIdx = Math.floor(Math.random()* quotes.length)
+    if (usedIndexes.has(randomIdx)) continue
+
+    const quote = quotes [randomIdx]
+    quoteElement.innerHTML = quote;
+    usedIndexes.add(randomIdx)
+    break
+}
+
+}
