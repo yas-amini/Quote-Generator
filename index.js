655d9b07a784aa800527c683b1c0fbed79004214
Yasamin <yaasamin.amini@gmail.com>
2024-08-12 13:34:18 +0200

    Fixed JS error to ensure DOM is loaded before running the script

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const quotes = [
-    “Well-behaved women seldom make history.”,
-    “Who knows what women can be when they are finally free to become themselves",
-    “Each time a woman stands up for herself, without knowing it possibly, without claiming it, she stands up for all women.”,
-    “When someone shows you who they are, believe them the first time.” ,
-    “Love costs all we are and will ever be. Yet it is only love which sets us free.”
+    "Well-behaved women seldom make history.",
+    "Who knows what women can be when they are finally free to become themselves.",
+    "Each time a woman stands up for herself, without knowing it possibly, without claiming it, she stands up for all women.",
+    "When someone shows you who they are, believe them the first time.",
+    "Love costs all we are and will ever be. Yet it is only love which sets us free."
 ]
 
-const usedIndexes = new Set()
-const quoteElement = document.getElementById("quote")
-function generateQuote(){
-   while (true)  {    
-    const randomIdx = Math.floor(Math.random()* quotes.length)
-    if (usedIndexes.has(randomIdx)) continue
+const usedIndexes = new Set();
 
-    const quote = quotes [randomIdx]
-    quoteElement.innerHTML = quote;
-    usedIndexes.add(randomIdx)
-    break
-}  
+function generateQuote() {
+    const quoteElement = document.getElementById("quote");
 
+    if (usedIndexes.size >= quotes.length) {
+        usedIndexes.clear();
+    }
+
+    while (true) {
+        const randomIdx = Math.floor(Math.random() * quotes.length);
+
+        if (usedIndexes.has(randomIdx)) continue;
+
+        const quote = quotes[randomIdx];
+        quoteElement.innerHTML = quote;
+        usedIndexes.add(randomIdx);
+        break;
+    }
 }
